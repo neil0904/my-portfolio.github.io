71c4f818f761d16b28e708c2551779287bd2d6d7
neil0904 <99093936+neil0904@users.noreply.github.com>
2023-07-14 19:27:25 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 //Show/Hide navbar
+let collapse = collapse navbar-collapse;
+let nav= nav;
 function toggleHide(){
   if(document.getElementsByClassName('nav navbar-nav') .style.display == 'block'){
     document.getElementsByClassName('nav navbar-nav') .style.display = 'none';
